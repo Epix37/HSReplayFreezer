acbe02738eda37a6e9ab44684fd38a2d1def5993
Alexander Zeier <epikz37@gmail.com>
2016-03-08 18:58:04 +0100

    revert to using default cxfreeze script

--- build.bat
+++ build.bat
@@ -1,19 +1,16 @@
+set CX_FREEZE=C:\Python34\Scripts\cxfreeze
 set /p VERSION="Build version: "
 git clone https://github.com/HearthSim/HSReplay
 cd HSReplay\python
 git clone https://github.com/HearthSim/python-hearthstone
 mv python-hearthstone\hearthstone hearthstone
 python setup.py install
-cd ..\..
-xcopy /y cx_freeze.py HSReplay\python\cx_freeze.py*
-xcopy /y hearthsim.ico HSReplay\python\hearthsim.ico*
-cd HSReplay\python
-python cx_freeze.py build
+python %CX_FREEZE% convert.py
 cd ..\..
 mkdir build
-mv HSReplay\python\build\exe.win32-3.4\hsreplayconverter.exe build\hsreplayconverter.exe
-mv HSReplay\python\build\exe.win32-3.4\pyexpat.pyd build\pyexpat.pyd
-mv HSReplay\python\build\exe.win32-3.4\python34.dll build\python34.dll
+mv HSReplay\python\dist\convert.exe build\hsreplayconverter.exe
+mv HSReplay\python\dist\pyexpat.pyd build\pyexpat.pyd
+mv HSReplay\python\dist\python34.dll build\python34.dll
 rd HSReplay /s /q
 @echo %VERSION%>> build/version
 python create_zip.py %VERSION%
